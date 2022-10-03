697a10d76d86fe8fc3951f866183a8c97ddfb3c1
Shivang Mangal <shivangmangal@gmail.com>
2022-10-04 00:21:40 +0530

    threadIdentifier.cpp MODIFIED

--- threadIdentifier.cpp
+++ threadIdentifier.cpp
@@ -11,6 +11,7 @@ int32_t main()
     std::thread t1(hello);
     std::thread t2(hello);
     cout << t1.get_id() << " " << t2.get_id() << endl;
+    cout << typeid(t1.get_id()).name() << endl;
     t1.join();
     t2.join();
 }
